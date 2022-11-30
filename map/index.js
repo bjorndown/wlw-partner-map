1a0e8df14715a8c3f6d98a76a0d533d433dfd379
Björn Mosler <dev@bjoern.mosler.ch>
2022-11-30 11:59:46 +0100

    map: improve readbility.

--- map/index.js
+++ map/index.js
@@ -1,14 +1,13 @@
-const response = await fetch('/geojson/partners.geojson')
-
-if (!response.ok) {
-  document.querySelector('#error').textContent = 'Could not load GeoJSON data'
-}
-
-const data = await response.json()
-
 const merenschwandCenter = [47.25941, 8.37492]
 const initialZoomLevel = 10
-const map = L.map('map').setView(merenschwandCenter, initialZoomLevel)
+const maxZoom = 19
+
+const statusToColor = {
+  P: 'green',
+  K: 'purple',
+  I: 'red',
+  unknown: '#BBB',
+}
 
 const renderPopup = layer => {
   const { NAME, url, comment } = layer.feature.properties
@@ -18,21 +17,30 @@ const renderPopup = layer => {
 }
 
 const style = feature => {
-  const { status } = feature.properties
-  const color = status === 'P' ? 'green' : status === 'I' ? 'red' : '#BBB'
-
-  const opacity = status === 'P' ? 1 : 0.5
+  const status = feature.properties.status ?? 'unknown'
+  const color = statusToColor[status]
+  const opacity = ['P', 'K'].includes(status) ? 1 : 0.5
   return { color, opacity }
 }
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map)
+const response = await fetch('/geojson/partners.geojson')
 
-L.geoJSON(data, {
-  style,
-})
-  .bindPopup(renderPopup)
-  .addTo(map)
+if (response.ok) {
+  const data = await response.json()
+
+  const map = L.map('map').setView(merenschwandCenter, initialZoomLevel)
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map)
+
+  L.geoJSON(data, {
+    style,
+  })
+    .bindPopup(renderPopup)
+    .addTo(map)
+} else {
+  document.querySelector('#error').textContent = 'Could not load partner data'
+}
